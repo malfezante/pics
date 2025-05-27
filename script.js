3e4cdaf68b1d6130b7b74487c62037c77595c1f5
malfezante <zefa.cnct@gmail.com>
2025-05-27 19:48:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function checkPassword() {
+  const input = document.getElementById('password').value;
+  const message = document.getElementById('message');
+
+  const correctPassword = "Puckissocute:3"; 
+
+  if (input === correctPassword) {
+    window.location.href = "memory"; // ← Le nom de la page secrète (si tu changes le nom du fichier)
+  } else {
+    message.textContent = "wrong"; // 
+  }
+}
